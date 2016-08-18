779587c752103325a1491186c194661ec9f3b5c9
eduardosouz <dudu.suporte@gmail.com>
2016-08-18 10:57:33 -0300

    Add files via upload

---
+++ Questão 2.py
@@ -0,0 +1,20 @@
+#Texto em Original#
+texto = 'eu tenho um sonho. O sonho de ver meus filhos julgados por sua personalidade, não pela cor de sua pele.'
+
+#Texto Original#
+print('\n''Questão 2')
+print('Texto Original: ',texto)
+
+#Texto em Maiusculo#
+print('\n''Questão 2 Letra A')
+print('Texto todo em Maiusculo: ', texto.upper())
+
+
+#Texto em Minusculo#
+print('\n''Questão 2 Letra B')
+print('Texto todo em Minusculo: ', texto.lower())
+
+
+#Texto a Primeira Maiuscula#
+print('\n''Questão 2 Letra C')
+print('Texto todo em Primeira em Maiusculo: ', texto.title())
---
+++ Questão 3.py
@@ -0,0 +1,25 @@
+#Numeros da questão#
+n1 = 200
+n2 = 250
+n3 = 8
+n4 = 7
+n5 = 10
+n6 = 800
+n7 = 2
+
+#Operações#
+soma = (n1 + n2)
+divisao = (n1 / n3)
+mutiplicacao = (n1 * n4)
+subtracao = (n6 - n5)
+potencia = (n7 ** n5)
+
+#Numeros a Ser Trabalhados#
+print('Numeros = ',n1,',',n2,',',n3,',',n4,',',n5,',',n6,',',n7)
+
+#Resolução dos Problemas#
+print('\n''Resposta da letra A, Soma dos numeros',n1, '+',n2,'é igual:',soma)
+print('\n''Resposta da letra B, Divisão dos numeros',n1, '/',n3,'é igual:',divisao)
+print('\n''Resposta da letra C, Mutiplicação dos numeros',n1, 'x',n4,'é igual:', mutiplicacao)
+print('\n''Resposta da letra D, Subtração dos numeros',n6, '-',n5,'é igual:', subtracao)
+print('\n''Resposta da letra E, Potencia dos numeros de',n7, '^',n5,'é igual:', potencia)
